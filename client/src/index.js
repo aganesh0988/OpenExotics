23c0d76e96da67b5c89cd86b8fb291ec9ef3b4e7
Andrea <aganesh0988@gmail.com>
2020-11-18 14:24:36 -0600

    cleaned up unnecessary store

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-// import AppWithContext from './components/AppWithContext';
-import configureStore from './store/configureStore';
 import App from './App';
 
-const store = configureStore();
+// const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-        {/* <AppWithContext /> */}
-      </Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
