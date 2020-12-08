32a5137affbf2c174f13d6aadf1fc5d621ee09b1
Andrea <aganesh0988@gmail.com>
2020-12-07 22:24:45 -0600

    added target blank to footer a's

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,16 +8,16 @@ const Footer = props => {
     return (
         <footer className="footer-container">
             <div className="footer-container__elements">
-                <a className="footer-container__elements-icons" href="https://andreagjackson.com/">
+                <a target="_blank" className="footer-container__elements-icons" href="https://andreagjackson.com/">
                     <h5>Designed & Developed by Andrea Jackson</h5>
                 </a>
-                <a className="footer-container__elements-icons" href="https://www.linkedin.com/in/andrea-jackson1/">
+                <a target="_blank" className="footer-container__elements-icons" href="https://www.linkedin.com/in/andrea-jackson1/">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
-                <a className="footer-container__elements-icons" href="https://angel.co/u/andrea-jackson-13">
+                <a target="_blank" className="footer-container__elements-icons" href="https://angel.co/u/andrea-jackson-13">
                     <FontAwesomeIcon icon={faAngellist} />
                 </a>
-                <a className="footer-container__elements-icons" href="https://github.com/aganesh0988">
+                <a target="_blank" className="footer-container__elements-icons" href="https://github.com/aganesh0988">
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
             </div>
