50b80995644c25fbf651c8fba31fb8faff3e3a58
Andrea <aganesh0988@gmail.com>
2021-02-01 22:01:08 -0600

    exported component

--- client/src/components/ReservationsProfile.js
+++ client/src/components/ReservationsProfile.js
@@ -1,3 +1,9 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom'
 import AuthContext from '../auth'
+
+const ReservationsProfile = () => {
+
+}
+
+export default ReservationsProfile
