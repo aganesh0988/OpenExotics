545d08d7daf2c9e23a8a7328475a34eb7754bdba
Andrea <aganesh0988@gmail.com>
2021-02-04 22:55:12 -0600

    added props and export

--- client/src/components/ResProfLayout.js
+++ client/src/components/ResProfLayout.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+
+const ResProfLayout = (props) => {
+    const { reservation } = props;
+
+
+}
+
+export default ResProfLayout;
