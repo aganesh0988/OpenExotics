fa502b67fd8a11755d51b5bdb76bf3613fd32957
Andrea <aganesh0988@gmail.com>
2021-02-05 12:17:32 -0600

    imported ResProfLayout to ReservationsProfile

--- client/src/components/ReservationsProfile.js
+++ client/src/components/ReservationsProfile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom'
+import ResProfLayout from './ResProfLayout'
 
 const ReservationsProfile = () => {
     const history = useHistory()
